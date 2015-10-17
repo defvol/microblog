6abe4f062ce837adc387d5a363d40e2018519a91
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-10-16 23:16:39 -0500

    Refactor static_pages test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Tuiter"
+  end
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Tuiter"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Tuiter"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Tuiter"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
