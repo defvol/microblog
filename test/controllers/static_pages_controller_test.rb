eedac8b2cc731f024430b7b67da92a9021b8340f
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-10-16 23:36:01 -0500

    Add a helper to generate page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,11 @@ class StaticPagesControllerTest < ActionController::TestCase
   def setup
     @base_title = "Tuiter"
   end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
