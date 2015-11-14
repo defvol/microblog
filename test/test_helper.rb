ede0ce86e97caaea55b8076f6afe2157d56d56e4
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-11-13 22:16:12 -0600

    Test application helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,5 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
