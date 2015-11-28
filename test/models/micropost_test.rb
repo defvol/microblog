ae000a5bb217e8a5140c2472037f56ebac066e9c
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-11-28 01:51:46 -0600

    Associate User to many Microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:rod)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
