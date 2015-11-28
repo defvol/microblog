cd0d9fae7f2656826d99a86d95e6a6a940c99744
Rod Wilhelmy <rwilhelmy@gmail.com>
2015-11-28 02:37:44 -0600

    Should be logged in to create or delete microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,15 +31,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email,
                                    :password, :password_confirmation)
     end
-
-    # Before filters
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 end
